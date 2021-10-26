fd1b1ebeb7852652192ad8094d3993e6a4dedab0
chrisgustafson-okta <53533241+chrisgustafson-okta@users.noreply.github.com>
2021-10-26 14:53:23 -0500

    added guard start

--- gusww-app/src/app/app-routing.module.ts
+++ gusww-app/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { InfluencerComponent } from './influencer/influencer.component';
+import { StaffComponent } from './staff/staff.component';
+import { AuthGuard } from './auth.guard';
 
-const routes: Routes = [  { path: 'home-component', component: HomeComponent },];
+const routes: Routes = [  
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'influencer', component: InfluencerComponent, canActivate: [AuthGuard]},
+  { path: 'staff', component: StaffComponent, canActivate: [AuthGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- gusww-app/src/app/app.module.ts
+++ gusww-app/src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+
+import { AuthService } from './auth.service';
+
+import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { InfluencerComponent } from './influencer/influencer.component';
+import { StaffComponent } from './staff/staff.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    InfluencerComponent,
+    StaffComponent
   ],
   imports: [
     MatSliderModule,
@@ -31,7 +40,7 @@ import { HomeComponent } from './home/home.component';
     MatIconModule,
     MatListModule,    
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
