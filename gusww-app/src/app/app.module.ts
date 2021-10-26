878f52c7df946b182660c5417207ffd8f4741c5f
chrisgustafson-okta <53533241+chrisgustafson-okta@users.noreply.github.com>
2021-10-26 12:35:05 -0500

    corrected routing

--- gusww-app/src/app/app.module.ts
+++ gusww-app/src/app/app.module.ts
@@ -12,10 +12,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MainNavComponent,
+    HomeComponent
   ],
   imports: [
     MatSliderModule,
