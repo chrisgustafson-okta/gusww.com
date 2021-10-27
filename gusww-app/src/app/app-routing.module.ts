1c042f0cd01823cc0b3794c436a6fa44e92590ea
chrisgustafson-okta <53533241+chrisgustafson-okta@users.noreply.github.com>
2021-10-27 09:50:36 -0500

    Okta wip

--- gusww-app/src/app/app-routing.module.ts
+++ gusww-app/src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { AboutComponent } from './about/about.component';
 import { InfluencerComponent } from './influencer/influencer.component';
 import { StaffComponent } from './staff/staff.component';
 import { AuthGuard } from './auth.guard';
+import { OktaAuthGuard } from '@okta/okta-angular';
+import { OktaCallbackComponent } from '@okta/okta-angular';
 
 const routes: Routes = [  
+  { path: 'login/callback', component: OktaCallbackComponent },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'influencer', component: InfluencerComponent, canActivate: [AuthGuard]},
-  { path: 'staff', component: StaffComponent, canActivate: [AuthGuard]}
+  { path: 'influencer', component: InfluencerComponent, canActivate: [OktaAuthGuard]},
+  { path: 'staff', component: StaffComponent, canActivate: [OktaAuthGuard]},  
 ];
 
 @NgModule({
--- gusww-app/src/app/app.module.ts
+++ gusww-app/src/app/app.module.ts
@@ -9,7 +9,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { OKTA_CONFIG, OktaAuthModule } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 
+const config = {
+  issuer: 'https://auth.gusww-demo.com',
+  clientId: '0oa4we06joofEMUTx696',
+  redirectUri: '/login/callback',
+  scopes: ['openid', 'profile', 'email'],
+  pkce: true  
+}
+const oktaAuth = new OktaAuth(config);
 import { AuthService } from './auth.service';
 
 import { AppComponent } from './app.component';
@@ -18,6 +28,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { InfluencerComponent } from './influencer/influencer.component';
 import { StaffComponent } from './staff/staff.component';
+
 
 @NgModule({
   declarations: [
@@ -38,9 +49,13 @@ import { StaffComponent } from './staff/staff.component';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule,    
+    MatListModule,   
+    OktaAuthModule 
   ],
-  providers: [AuthService],
+  providers: [AuthService, { 
+    provide: OKTA_CONFIG, 
+    useValue: { oktaAuth } 
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
