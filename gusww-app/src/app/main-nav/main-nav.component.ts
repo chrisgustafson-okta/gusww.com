3fa2b46f3b9d074e43e13e88dddfd1550e542002
chrisgustafson-okta <53533241+chrisgustafson-okta@users.noreply.github.com>
2021-10-29 08:37:53 -0500

    Claim access

--- gusww-app/src/app/main-nav/main-nav.component.ts
+++ gusww-app/src/app/main-nav/main-nav.component.ts
@@ -2,20 +2,41 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 
+interface Claim {
+  claim: string;
+  value: string;
+}
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css']
 })
 export class MainNavComponent {
-
+  claims: Claim[] = [];
+  
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver,  public authStateService: OktaAuthStateService,  public oktaAuth: OktaAuth) {}
+
+  async ngOnInit() {
+ 
+    const userClaims = await this.oktaAuth.getUser();
+    this.claims = Object.entries(userClaims).map(entry => ({ claim: entry[0], value: entry[1] }));
+  }
+
+  async login() {
+    await this.oktaAuth.signInWithRedirect();
+  }
+
+  async logout() {
+    await this.oktaAuth.signOut();
+  }
 
 }
